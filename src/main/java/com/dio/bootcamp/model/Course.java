0f358274dbbf335ed2d65e3f79857b549b99924c
Yves de Sá <yvesdsb98@gmail.com>
2022-05-15 16:54:12 -0300

    feat: created application model

---
+++ src/main/java/com/dio/bootcamp/model/Course.java
@@ -0,0 +1,28 @@
+package com.dio.bootcamp.model;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Data;
+
+@Entity(name = "tb_course")
+@Data
+public class Course {
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+  @Column(name = "id", columnDefinition = "VARCHAR(255)")
+  private UUID id;
+
+  private String title;
+
+  private String description;
+
+  private int workload;
+}
